505c5b4991bf54b1ba52f07b5101a6bf8aebdef6
maru4737 <maru4737@naver.com>
2024-06-04 16:43:52 +0900

    DB 커넥트

--- src/main/java/com/example/joy/login/daoImpl/LoginDaoImpl.java
+++ src/main/java/com/example/joy/login/daoImpl/LoginDaoImpl.java
@@ -2,18 +2,26 @@ package com.example.joy.login.daoImpl;
 
 import com.example.joy.login.dao.LoginDao;
 import com.example.joy.login.vo.userVo;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class LoginDaoImpl implements LoginDao {
-
-
+public class LoginDaoImpl extends SqlSessionDaoSupport implements LoginDao {
+    @Autowired
+    public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory){
+        super.setSqlSessionFactory(sqlSessionFactory);
+    }
     @Override
     public userVo usrLogin(userVo usrVo) {
 
         System.out.println("<<LoginDaoImpl>>");
         System.out.println(usrVo.toString());
+        System.out.println((userVo) getSqlSession().selectOne("LoginDaoImpl.selectTestSample", usrVo));
+
 
         return null;
+
     }
 }
